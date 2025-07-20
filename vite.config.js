eb3f5bd21a2d4d32e01ce322a1572436e75fca7a
Hamza <sahamzashah19@gmail.com>
2025-07-20 15:19:20 +0500

    feat: add server configuration to Vite for improved development setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,      // 👈 binds to 0.0.0.0
+    port: 5173,      // optional, just to be sure
+    strictPort: true // avoid port auto-switching
+  }
 })
